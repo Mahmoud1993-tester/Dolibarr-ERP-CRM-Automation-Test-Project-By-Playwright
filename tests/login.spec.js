d971c5d28278999f2f942c74f48cc324eabc2030
Mahmoud Saber <msaber93.egy@gmail.com>
2025-05-10 21:36:46 +0300

    Login Test for Dolibarr Website

---
+++ tests/login.spec.js
@@ -0,0 +1,23 @@
+// @ts-check
+const {test, expect} = require ('@playwright/test')
+
+test("Valid Login", async({page}) => {
+    // Navigate to Login Page
+    await page.goto("https://demo.dolibarr.org/public/demo/index.php")
+    await page.click("div.demothumbtext")
+    await expect(page).toHaveURL(/urlfrom/)
+
+    // Login Page 
+    await page.fill("#username", "demo")
+    await page.fill("#password", "demo")
+    await page.locator("input.button").click()
+
+    // Wait Profile Page 
+    await page.waitForTimeout(5000)
+
+    // Profile Page check
+    await expect(page).toHaveURL(/mainmenu=home/)
+    const dashboardCheck = page.locator("div.menu_titre").first()
+    await expect(dashboardCheck).toHaveText("My Dashboard")
+
+} )
